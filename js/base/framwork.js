6c7d25fa1a032bec45dd92ee999f1cf813ffc30c
page <page@be-xx.com>
2020-07-01 15:43:18 +0800

    解决安卓放大字体的问题

--- js/base/framwork.js
+++ js/base/framwork.js
@@ -31,3 +31,29 @@ if(ibase.debug){
 	ibase.load('js/base/vConsole.min.js');
 	window.console.table(ibase.screenList);
 }//end if
+
+/**
+ * 安卓
+ */
+function androidFontLock(){
+	if(os.android){
+		if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+			handleFontSize();
+		} else {
+			if (document.addEventListener) {
+				document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+			} else if (document.attachEvent) {
+				document.attachEvent("WeixinJSBridgeReady", handleFontSize);
+				document.attachEvent("onWeixinJSBridgeReady", handleFontSize);  }
+		}
+		function handleFontSize() {
+			// 设置网页字体为默认大小
+			WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+			// 重写设置网页字体大小的事件
+			WeixinJSBridge.on('menu:setfont', function() {
+				WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+			});
+		}
+	}
+}
+androidFontLock();
